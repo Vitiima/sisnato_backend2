0eb8e1e3589f10977d8d644758394cf0d61690b1
V. Matheus Almeida <matheus_famas@hotmail.com>
2025-04-08 14:34:47 -0300

    get-info

--- src/api/get-infos/get-infos.controller.spec.ts
+++ src/api/get-infos/get-infos.controller.spec.ts
@@ -1,20 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GetInfosController } from './get-infos.controller';
 import { GetInfosService } from './get-infos.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../prisma/prisma.service';
 
 describe('GetInfosController', () => {
   let controller: GetInfosController;
+  let service: GetInfosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GetInfosController],
-      providers: [GetInfosService],
+      providers: [{
+        provide: GetInfosService,
+        useValue: {
+          getTermos: jest.fn(),
+          getPoliticas: jest.fn(),
+        }
+      },
+      {
+        provide: PrismaService,
+        useValue: {}
+      },
+      {
+        provide: JwtService,
+        useValue: {}
+      }
+      ],
     }).compile();
 
     controller = module.get<GetInfosController>(GetInfosController);
+    service = module.get<GetInfosService>(GetInfosService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+
+  describe('getTermos', () => {
+    it('should be defined', () => {
+      expect(controller.getTermos).toBeDefined();
+    });
+    it('should return fail', async () => {
+      jest.spyOn(service, 'getTermos').mockRejectedValue(new Error('Error getting termos'));
+      await expect(controller.getTermos()).rejects.toThrowError('Error getting termos');
+    });
+  })
+
+  describe('getPoliticas', () => {
+    it('should be defined', () => {
+      expect(controller.getPoliticas).toBeDefined();
+    });
+    it('should return fail', async () => {
+      jest.spyOn(service, 'getPoliticas').mockRejectedValue(new Error('Error getting politicas'));
+      await expect(controller.getPoliticas()).rejects.toThrowError('Error getting politicas');
+    });
+  })
 });
--- src/api/get-infos/get-infos.service.ts
+++ src/api/get-infos/get-infos.service.ts
@@ -7,31 +7,7 @@ import { plainToClass } from 'class-transformer';
 @Injectable()
 export class GetInfosService {
   constructor(private prismaService: PrismaService) {}
-  async checkCpf(cpf: string): Promise<boolean> {
-    try {
-      const Exist = await this.prismaService.solicitacao.findFirst({
-        where: {
-          cpf: cpf,
-        },
-      });
 
-      if (Exist) {
-        const retorno: GetInfoErrorEntity = {
-          message: 'Cpf ja cadastrado',
-        };
-        throw new HttpException(retorno, 400);
-      }
-
-      return false;
-    } catch (error) {
-      const retorno: GetInfoErrorEntity = {
-        message: 'ERRO DESCONHECIDO',
-      };
-      throw new HttpException(retorno, 500);
-    } finally {
-      await this.prismaService.$disconnect();
-    }
-  }
 
   async getTermos() {
     try {
